539c748e593ae63dc5c825b592ecaa3c592e6bb8
SilenceRichard <silencerichard@163.com>
2023-05-22 12:46:38 +0800

    update

---
+++ app.js
@@ -0,0 +1,38 @@
+// 导入所需的库
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors())
+
+// 设置端口号
+const PORT = 3001;
+const { OpenAIStream } = require('./OpenAIStream');
+
+// 设置路由
+app.post('/lessonPlan', async (req, res) => {
+  const model = {
+    id: 'gpt-3.5-turbo',
+    name: 'gpt-3.5-turbo'
+  }
+  const promptToSend = `你是一个教案生成器，无需为我回复其他，只需根据用户要求按标准教案格式回复，在每个二级标题下面插入生成的内容，一份标准的教案格式如下：
+    # 教案示例
+    ## 一、教材分析
+    ## 二、教学目标
+    ## 三、教学重点
+    ## 四、教学难点
+    ## 五、课时安排
+    ## 六、教学资源准备
+    ## 七、教学过程
+  `
+  const temperatureToUse  = 0;
+    // 获取请求参数
+  const queryParams = req.body;
+  const messagesToSend = queryParams.messags;
+  const stream = await OpenAIStream(model, promptToSend, temperatureToUse, '', messagesToSend);
+  res.send(stream)
+});
+
+// 启动服务器
+app.listen(PORT, () => {
+  console.log(`服务器正在运行在端口 ${PORT}`);
+});
---
+++ openAIStream.js
@@ -0,0 +1,113 @@
+const { createParser } = require('eventsource-parser');
+
+class OpenAIError extends Error {
+  constructor(message, type, param, code) {
+    super(message);
+    this.name = 'OpenAIError';
+    this.type = type;
+    this.param = param;
+    this.code = code;
+  }
+}
+
+const OPENAI_API_TYPE = 'openai';
+const AZURE_DEPLOYMENT_ID = '';
+const OPENAI_API_HOST = 'https://api.openai.com';
+
+const OpenAIStream = async (
+  model,
+  systemPrompt,
+  temperature,
+  key,
+  messages,
+) => {
+  let url = `${OPENAI_API_HOST}/v1/chat/completions`;
+  if (OPENAI_API_TYPE === 'azure') {
+    url = `${OPENAI_API_HOST}/openai/deployments/${AZURE_DEPLOYMENT_ID}/chat/completions?api-version=${OPENAI_API_VERSION}`;
+  }
+  const res = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...(OPENAI_API_TYPE === 'openai' && {
+        Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`
+      }),
+      ...(OPENAI_API_TYPE === 'azure' && {
+        'api-key': `${key ? key : process.env.OPENAI_API_KEY}`
+      }),
+      ...((OPENAI_API_TYPE === 'openai' && OPENAI_ORGANIZATION) && {
+        'OpenAI-Organization': OPENAI_ORGANIZATION,
+      }),
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      ...(OPENAI_API_TYPE === 'openai' && {model: model.id}),
+      messages: [
+        {
+          role: 'system',
+          content: systemPrompt,
+        },
+        ...messages,
+      ],
+      max_tokens: 1000,
+      temperature: temperature,
+      stream: true,
+    }),
+  });
+
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  if (res.status !== 200) {
+    const result = await res.json();
+    if (result.error) {
+      throw new OpenAIError(
+        result.error.message,
+        result.error.type,
+        result.error.param,
+        result.error.code,
+      );
+    } else {
+      throw new Error(
+        `OpenAI API returned an error: ${
+          decoder.decode(result?.value) || result.statusText
+        }`,
+      );
+    }
+  }
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const onParse = (event) => {
+        if (event.type === 'event') {
+          const data = event.data;
+
+          try {
+            const json = JSON.parse(data);
+            if (json.choices[0].finish_reason != null) {
+              controller.close();
+              return;
+            }
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
+        }
+      };
+
+      const parser = createParser(onParse);
+
+      for await (const chunk of res.body) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+};
+
+module.exports = {
+  OpenAIError,
+  OpenAIStream,
+};
