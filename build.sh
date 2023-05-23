1cf7b3055b05c63f269d83dfa356e369a710dcc6
SilenceRichard <silencerichard@163.com>
2023-05-23 23:22:45 +0800

    update

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script will copy all files from the current directory to the specified remote directory on the server
+
+# Set variables
+remote_user="ubuntu"
+remote_ip="43.134.126.166"
+remote_directory="/home/ubuntu/codes/demo-lesson-planner-server"
+
+# Copy files to remote server
+scp -r * ${remote_user}@${remote_ip}:${remote_directory}
