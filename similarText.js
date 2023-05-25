bda232c60ac43f20591733d0cf4539f88dcaa4ff
SilenceRichard <silencerichard@163.com>
2023-05-25 20:09:53 +0800

    update

--- similarText.js
+++ similarText.js
@@ -1,31 +1,23 @@
 // @see https://docs.aircode.io/guide/functions/
-const createVectorStore = require('./createVectorStore');
-const OpenAI = require('langchain/llms/openai').OpenAI;
-const { VectorDBQAChain, ConversationalRetrievalQAChain, loadSummarizationChain, AnalyzeDocumentChain, LLMChain,  } = require("langchain/chains")
 const { v4: uuidv4 } = require('uuid');
+// 根据教学资源、教学主题锁定课文
+const getLessonText = async (params) => {
+  const res = await fetch(`https://y42h83s1qy.hk.aircode.run/getLessonContent?textBookName=${params.textBookName}&teachingTheme=${params.teachingTheme}`)
+  const text = await res.text();
+  return text;
+}
 
 module.exports = async function (params, context) {
   console.log('Received params:', params);
-   const { teachingTheme: theme, name } = params;
-  const chineseLessonVectorStore = await createVectorStore({
-    collectionName: 'lesson',
-    classify: 'Chinese',
-    name
+  // name: 教学资源 teachingTheme: 教学主题
+  const { textBookName, teachingTheme } = params;
+  const lessonText = await getLessonText({
+    "textBookName": textBookName,
+    "teachingTheme": teachingTheme
   })
-  let similarLesson
-  let similarLessonText
-  let summaryLessonText = {text: ''};
-  const textModel = new OpenAI({ temperature: 0 });
-  if (theme) {
-     similarLesson = await chineseLessonVectorStore.similaritySearch(`${theme}`, 3);
-    const chain = loadSummarizationChain(textModel, { type: "map_reduce" });
-    summaryLessonText = await chain.call({
-      input_documents: similarLesson,
-    });
-  }
-  console.log(summaryLessonText)
+  
   return {
-    text: summaryLessonText.text,
+    text: lessonText,
     recordId: uuidv4(), // 返回的id作为轮询id
   };
 };
