218f5e86ceb5f175b5f6f73e8167568051110a31
SilenceRichard <silencerichard@163.com>
2023-05-26 03:55:39 +0800

    更新

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // 导入所需的库
 const express = require('express');
 const cors = require('cors');
-const { Readable } = require('stream');
 // 导入dotenv库
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,34 +11,10 @@ app.use(express.json());
 
 // 设置端口号
 const PORT = 3001;
-const { OpenAIStream } = require('./openAIStream');
 const similarText = require('./similarText');
-// 设置路由
-app.post('/lessonPlan', async (req, res) => {
-  const model = {
-    id: 'gpt-3.5-turbo',
-    name: 'gpt-3.5-turbo'
-  }
-  const promptToSend = `你是一个教案生成器，无需为我回复其他，只需根据用户要求按标准教案格式回复，在每个二级标题下面按统一格式插入生成的内容，一份标准的教案格式如下：
-    # 教案示例
-    ## 一、教材分析
-    ## 二、教学目标
-    ## 三、教学重点
-    ## 四、教学难点
-    ## 五、课时安排
-    ## 六、教学资源准备
-    ## 七、教学过程
-  `
-  const temperatureToUse = 0;
-  // 获取请求参数
-  const queryParams = req.body;
-  const messagesToSend = queryParams.messages;
-  const stream = await OpenAIStream(model, promptToSend, temperatureToUse, '', messagesToSend);
-  // 将ReadableStream对象转换为Readable流
-  const nodeStream = Readable.from(stream);
-  // 将Readable流发送给前端
-  nodeStream.pipe(res);
-});
+const genLessonPlan = require('./genLessonPlan');
+// 生成教案
+app.post('/lessonPlan', genLessonPlan);
 
 app.get('/similarText', async(req, res) => {
   const params = req.query;
---
+++ genLessonPlan.js
@@ -0,0 +1,71 @@
+const { Readable } = require('stream');
+const { OpenAIStream } = require('./openAIStream');
+
+const saveToDb = async (data) => {
+  await fetch('https://y42h83s1qy.hk.aircode.run/saveLessonPlanner', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
+
+const genLessonPlan = async (req, res) => {
+  const model = {
+    id: 'gpt-3.5-turbo',
+    name: 'gpt-3.5-turbo'
+  }
+  const promptToSend = `你是一个教案生成器，无需为我回复其他，只需根据用户要求按标准教案格式回复，在每个二级标题下面按统一格式插入生成的内容，一份标准的教案格式如下：
+    # 教案示例
+    ## 一、教材分析
+    ## 二、教学目标
+    ## 三、教学重点
+    ## 四、教学难点
+    ## 五、课时安排
+    ## 六、教学资源准备
+    ## 七、教学过程
+  `
+  const temperatureToUse = 0;
+  // 获取请求参数
+  const queryParams = req.body;
+  const messagesToSend = queryParams.messages;
+  const stream = await OpenAIStream(model, promptToSend, temperatureToUse, '', messagesToSend);
+  // 将ReadableStream对象转换为Readable流
+  const nodeStream = Readable.from(stream);
+   // Create an array to hold the data
+   let recordData = [];
+   // Listen for data events to collect the data chunks
+   nodeStream.on('data', chunk => {
+    recordData.push(chunk);
+   });
+ 
+   // Listen for end event to know when the stream has ended
+   nodeStream.on('end', async () => {
+     // Concatenate all chunks to form the complete data
+     const completeData = Buffer.concat(recordData).toString();
+     // Save the completeData to the database
+     await saveToDb({
+      lessonPlan: completeData,
+      teachingTheme: queryParams.teachingTheme,
+      textBookName: queryParams.textBookName
+     })
+     try {
+        // assuming you have a save function in your db module
+       console.log('Data saved to the database successfully');
+     } catch (error) {
+       console.error('Error saving data to the database', error);
+       await saveToDb({
+        teachingTheme: queryParams.teachingTheme,
+        textBookName: queryParams.textBookName,
+        error: error
+       })
+     }
+   });
+ 
+  // 将Readable流发送给前端
+  nodeStream.pipe(res);
+  
+}
+
+module.exports = genLessonPlan;
